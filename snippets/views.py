e41c02520c3207e456fff824088d66262394b0f8
Derli Machado <dam.machado1@gmail.com>
2021-03-05 07:09:54 -0300

    Change: SnippetsList to UserList to use authentication on access.

--- snippets/views.py
+++ snippets/views.py
@@ -1,13 +1,15 @@
-from snippets.models import Snippet
-from snippets.serializers import SnippetSerializer
-from rest_framework import generics
+from django.contrib.auth.models import User
+from snippets.serializers import UserSerializer
 
 
-class SnippetList(generics.ListCreateAPIView):
-    queryset = Snippet.objects.all()
-    serializer_class = SnippetSerializer
+class UserList(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
 
 
-class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Snippet.objects.all()
-    serializer_class = SnippetSerializer
+class UserDetail(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
